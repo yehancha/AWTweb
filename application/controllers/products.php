5854c329a7fae67dc2b9af980559dc400bf7b142
Yehan Pemarathne <yehancha@gmail.com>
2014-10-28 21:11:25 +0530

    Initial Commit

---
+++ application/controllers/products.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of products
+ *
+ * @author yehancha
+ */
+class Products extends CI_Controller {
+    public function index() {
+        //$this->load->
+    }
+}
---
+++ application/models/product_model.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of product_model
+ *
+ * @author yehancha
+ */
+class Product_model extends CI_Model {
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function lookup($productType) {
+        $this->db->where('name', $productType);
+        $results = $this->db->get('producttype');
+        $returnData = array();
+        foreach ($results->result() as $value) {
+            $returnData[] = $value->name;
+        }
+        return $returnData;
+    }
+}
